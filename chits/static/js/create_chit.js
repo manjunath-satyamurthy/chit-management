7f67760595426d29b47cf3ee307b53b05532e39b
Manjunath <pass2rahul@gmail.com>
2015-07-17 18:09:15 +0530

    replaced javascript files with cdn

--- chits/static/js/create_chit.js
+++ chits/static/js/create_chit.js
@@ -14,12 +14,32 @@ $(document).ready( function () {
     "bInfo": false,
   });
 
-  $('#myModal').on('show.bs.modal', function(e){
-    setTimeout( function(){
-      member_table.columns.adjust();
-      return null;
-    }, 200);
+  $('#create_chit_form').validator();
 
+  $('#create_chit_submit').click( function (e) {
+    console.log('cathed')
+    e.preventDefault();
+    $('#create_chit_form').validator('validate');
   });
 
+});
+
+$('#add_members_button').click( function (e) {
+  e.preventDefault();
+  $('#create_chit_form').validator('validate');
+  if ($('#no_members').hasClass('has-error') == true) {
+    console.log('no here')
+  }
+
+  else {
+    console.log('here')
+    $('#myModal').modal();
+  }
+});
+
+$('#myModal').on('show.bs.modal', function(e){
+  setTimeout( function(){
+    member_table.columns.adjust();
+    return null;
+  }, 200);
 });
