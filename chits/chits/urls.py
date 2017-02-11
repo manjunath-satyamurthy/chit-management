15e885cfdf627783537ede4dd2a891c8385aae69
manjunath-satyamurthy <pass2rahul@gmail.com>
2017-02-11 17:18:21 +0100

    Updating user email and name

--- chits/chits/urls.py
+++ chits/chits/urls.py
@@ -2,10 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'chits.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('base.urls')),
     url(r'', include('management.urls')),
